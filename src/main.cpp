a4a0fa44a5eb8ca725718b4303c1b30715bf3aec
Toby Shi <toby3030@qq.com>
2023-10-23 16:10:55 +0800

    [style] output files

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,9 @@ mir::Manager mir_manager;
 
 int main(int argc, char **argv) {
     std::ifstream fin(argc >= 2 ? argv[1] : "testfile.txt");
-    std::ofstream fout(argc >= 3 ? argv[2] : "output.txt");
-    std::ofstream ferr(argc >= 4 ? argv[3] : "error.txt");
+    std::ofstream fir(argc >= 3 ? argv[2] : "llvm_ir.txt");
+    std::ofstream fmips(argc >= 4 ? argv[3] : "mips.txt");
+    std::ofstream ferr(argc >= 5 ? argv[4] : "error.txt");
 
     std::string src, s;
     while (std::getline(fin, s)) {
@@ -30,7 +31,7 @@ int main(int argc, char **argv) {
 
     if (message_queue.empty()) {
         mir_manager.cleanPool();
-        mir_manager.output(fout);
+        mir_manager.output(fir);
     }
     return 0;
 }
